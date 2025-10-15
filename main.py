31c564214166c8dbddb8769e6b552d903c00e9f6
Shahan S Shetty <shahansshetty@gmail.com>
2025-10-15 20:43:37 +0530

    trying to add flask to this project

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from flask import Flask,render_template
 
+app=Flask(__name__)
 
+@app.route('/')
+def chat_bot():
+    return render_template('chat_bot.html')
 
 load_dotenv()
 
@@ -26,6 +31,7 @@ def chat(prompt):
     return response.choices[0].message.content
 
 if __name__=="__main__":
+    app.run(debug=True)
     while True:
         user_input=input("You: ")
         if user_input.lower() in ['quit','exit', 'bye']:
